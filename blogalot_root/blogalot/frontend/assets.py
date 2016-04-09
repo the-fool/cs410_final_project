0581774c3a49e9b600b7319d2a8ce11df5991ae0
the-fool <sketchbang@gmail.com>
2016-04-09 09:00:52 -0500

    still initing

--- blogalot_root/blogalot/frontend/assets.py
+++
@@ -1,8 +0,0 @@
-from flask_assets import Environment, Bundle
-
-css_blogalot = Bundle("scss/blogalot.scss",
-                      filters="scss", output="css/blogalot.css",
-                      debug=False)
-
-css_all = Bundle(css_blogalot, filters="cssmin", output="css/blogalot.min.css")
-
---
+++ blogalot_root/blogalot/frontend/dashboard.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+
+from . import route
+
+bp = Blueprint('dashboard', __name__)
+
+@route(bp, '/')
+def index():
+    return render_template('dashboard.html')
