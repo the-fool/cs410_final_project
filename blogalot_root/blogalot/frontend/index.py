81305c9a37fe09bc4a85276fdce5062405e7fb4c
the-fool <sketchbang@gmail.com>
2016-04-12 17:59:20 -0500

    now listing posts on homepage

--- blogalot_root/blogalot/frontend/index.py
+++ blogalot_root/blogalot/frontend/index.py
@@ -14,11 +14,14 @@ bp = Blueprint('index', __name__)
 
 @route(bp, '/', methods=['GET', 'POST'])
 def index():
-    form = NewPostForm()
+    form = NewPostForm(csrf_enabled=False)
     if form.validate_on_submit():
+        print(form.content.data)
         post = Post(content=form.content.data,
                     author=current_user._get_current_object())
         db.session.add(post)
+        db.session.commit()
         return redirect(url_for('.index'))
     posts = PostsService.all()
+    print(posts)
     return render_template('posts.html', posts=posts, new_post_form=form)
