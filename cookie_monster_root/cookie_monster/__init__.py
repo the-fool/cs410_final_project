fac235f62ca6c9194bc31c2b74c6daa8922b7366
the-fool <sketchbang@gmail.com>
2016-04-13 08:24:25 -0500

    add manager to cookie monster

--- cookie_monster_root/cookie_monster/__init__.py
+++ cookie_monster_root/cookie_monster/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
 
     app.config.from_object('cookie_monster.settings')
+
+    db.init_app(app)
     
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
