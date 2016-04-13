9e630a148339863a6447dbf037bb1acb0dd33e58
the-fool <sketchbang@gmail.com>
2016-04-12 19:39:00 -0500

    add manager commands re: posts

--- blogalot_root/blogalot/manage/posts.py
+++ blogalot_root/blogalot/manage/posts.py
@@ -1,9 +1,33 @@
 from flask.ext.script import Command, prompt
-from werkzeug.datastructures import MultiDict
+
+from ..services import users, posts
+from ..posts.models import Post
 
 class CreatePostCommand(Command):
 
     def run(self):
         content = prompt("Content")
         user_name = prompt("User name")
-        data = MultiDict(dict(content=content, user_name=user_name))
+        user = users.first(name=user_name)
+        if not user:
+            print("No user with name: {}".format(user_name))
+            return
+        new_post = Post(content=content, author=user)
+        posts.save(new_post)
+        
+class DeletePostCommand(Command):
+
+    def run(self):
+        id = prompt("ID")
+        to_delete = posts.get(id)
+        if not to_delete:
+            print("No post with id: {}".format(id))
+            return
+        posts.delete(to_delete)
+
+
+class ListPostsCommand(Command):
+
+    def run(self):
+        for p in posts.all():
+            print('Post (id={0}, content={1}'.format(p.id, p.content[:15]))
--- blogalot_root/manage.py
+++ blogalot_root/manage.py
@@ -3,13 +3,17 @@ from flask_migrate import MigrateCommand
 
 from blogalot.api import create_app
 from blogalot.manage import CreateUserCommand,\
-     DeleteUserCommand, ListUsersCommand
+     DeleteUserCommand, ListUsersCommand,\
+     DeletePostCommand, ListPostsCommand
 
 manager = Manager(create_app(register_security_blueprint=True))
 manager.add_command('create_user', CreateUserCommand())
 manager.add_command('delete_user', DeleteUserCommand())
 manager.add_command('list_users', ListUsersCommand())
 manager.add_command('db', MigrateCommand)
+manager.add_command('list_posts', ListPostsCommand())
+manager.add_command('delet_post', DeletePostCommand())
+
 
 if __name__ == "__main__":
     manager.run()
