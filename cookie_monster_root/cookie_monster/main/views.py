883a604e102b5efe617d20d70b1b6f7772cb59db
the-fool <sketchbang@gmail.com>
2016-04-15 12:10:48 -0500

    Store cookies in db

--- cookie_monster_root/cookie_monster/main/views.py
+++ cookie_monster_root/cookie_monster/main/views.py
@@ -1,8 +1,17 @@
 from flask import render_template, url_for, request
-
+from ..models import Cookie, db
 from . import main
 
 @main.route('/')
 def index():
-    print(request.args.get('cookies'))
-    return "Okie dokie"
+    cookies = request.args.get('cookies')
+    if not cookies:
+        return "Okie dokie"
+
+    cookies = [c.strip() for c in cookies.split(';')]
+    for c in cookies:
+        c = c.split('=')
+        cookie = Cookie(name=c[0], value=c[1])
+        db.session.add(cookie)
+    db.session.commit()
+    return ''
